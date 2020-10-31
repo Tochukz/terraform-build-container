e08eff1a24cce807dd3c7c05454aaa6caa9c2dac
Colin But <colin.but@sainsburys.co.uk>
2020-10-31 10:19:17 +0000

    added more examples to the main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -48,3 +48,17 @@ resource "aws_dynamodb_table" "query-store" {
 
   tags = module.label.tags
 }
+
+resource "aws_s3_bucket_public_access_block" "vpc_flowlogs_s3_bucket_public_access_block" {
+  bucket                  = aws_s3_bucket.vpc_flowlogs_s3_bucket.id
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+resource "aws_s3_bucket" "aqm_lambda_functions" {
+
+  bucket = "aqm-lambda-functions"
+  acl    = "private"
+}
